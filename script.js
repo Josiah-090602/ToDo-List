e8b3c0e7c90026a4b9007a0b37d94873d229a8b0
Josiah Viernes <viernesjosiah6@gmail.com>
2024-01-25 19:04:58 +0800

    Added: Delete Task Function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const tasksItem = document.getElementById('task-items')
 const taskDate = document.getElementById('taskDate')
 const taskPriority = document.getElementById('taskPriority')
 const taskNote = document.getElementById('taskNote')
+const tasksTitle = document.getElementById('tasksTitle')
 
 
 
@@ -16,8 +17,11 @@ addTask.addEventListener("click", () => {
         let listTask = document.createElement("li")
         listTask.textContent = taskNote.value
         tasksItem.appendChild(listTask)
+        tasksTitle.style.display = "flex"
+        addDate(listTask)
+        resetInputs()
     }
-    resetInputs()
+    
 })
 
 
@@ -26,4 +30,43 @@ const resetInputs = () =>{
     taskDate.value = ''
     taskNote.value = ''
     taskPriority.value = ''
+}
+
+
+//Remove Task
+tasksItem.addEventListener("click", (clicked) => {
+    if (clicked.target.tagName === "LI") {
+        clicked.target.style.backgroundColor = "#C1F2B0"
+    } else if (clicked.target.tagName === "IMG") {
+        clicked.target.parentElement.remove()
+    }
+})
+
+
+
+//add Date and Priority
+const addDate = (addedList) => {
+        let dateValue = document.createElement('span')
+        dateValue.textContent = taskDate.value
+        addedList.appendChild(dateValue)
+
+        let priority = document.createElement('div')
+        if (taskPriority.value === 'high') {
+            priority.style.backgroundColor = "var(--red)"
+        } else if (taskPriority.value === 'medium') {
+            priority.style.backgroundColor = "var(--yellow)"
+        } else {
+            priority.style.backgroundColor = "var(--green)"
+        }
+        addedList.appendChild(priority)
+
+        let deleteIcon = document.createElement('img')
+        deleteIcon.src = "/img/trash.svg"
+        addedList.appendChild(deleteIcon)
+}
+
+
+//Onload
+window.onload = () => {
+    tasksTitle.style.display = "none"
 }
